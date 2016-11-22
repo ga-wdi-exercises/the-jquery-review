35f0077327a8d745e4d6e4e218ada4a19c86eb57
yourUsername <your_email@example.com>
2016-11-22 08:58:53 -0500

    Attempted hw

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Begin writing your code after the variable declarations
-var staff = ["Richard Feynman", "Barefoot Contessa", "Patti Stanger", "Shania Twain Jr.", "Dean Ambrose", "Roger Lodge"];
+let staff = ["Richard Feynman", "Barefoot Contessa", "Patti Stanger", "Shania Twain Jr.", "Dean Ambrose", "Roger Lodge"];
 
-var articles = [
+
+let articles = [
   {
     header: {
       title: "Gibberish",
@@ -35,3 +36,18 @@ var articles = [
 ]
 
 // Your code below
+let arrayLength = staff.length;
+for (let i = 0; i < arrayLength; i++) {
+    console.log(staff[i]);
+}
+
+staffNames()
+function staffNames(){
+  for (let i=0; i<staff.length; i++){
+  span = '<span>'+staff[i]+'</span>';
+  $('main > header').append(span);
+  }
+}
+
+// need to add this line:
+// p = $('p').text('Shark far unsafely some hello a over hey overabundant rarely amiable so as surreptitious ceaseless busted flabbily as some quail jeepers some vibrantly house the wow wow hello far. Hey this hey concomitant when horrendous gull within far alas forgave thanks after poignantly tritely flamingo rarely one sullenly jocose. \n Why painful the sixteen how minuter looking nor. Subject but why ten earnest husband imagine sixteen brandon. Are unpleasing occasional celebrated motionless unaffected conviction out. Evil make to no five they. Stuff at avoid of sense small fully it whose an. Ten scarcely distance moreover handsome age although. As when have find fine or said no mile. He in dispatched in imprudence dissimilar be possession unreserved insensible. She evil face fine calm have now. Separate screened he outweigh of distance landlord. \n Sportsman do offending supported extremity breakfast by listening. Decisively advantages nor expression unpleasing she led met. Estate was tended ten boy nearer seemed. As so seeing latter he should thirty whence. Steepest speaking up attended it as. Made neat an on be gave show snug tore. ')
